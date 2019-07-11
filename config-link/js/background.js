ea344dc8691f3a20b9ff3515e29435603e9456a7
chenzejiang <820289461@qq.com>
2019-07-11 09:14:48 +0800

    增加background.js 监听页面URL 变化

---
+++ config-link/js/background.js
@@ -0,0 +1,27 @@
+// 获取当前选项卡ID
+function getCurrentTabId(callback)
+{
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs)
+	{
+		if(callback) callback(tabs.length ? tabs[0].id: null);
+	});
+}
+
+// 向content-script主动发送消息
+function sendMessageToContentScript(message, callback)
+{
+	getCurrentTabId((tabId) =>
+	{
+		chrome.tabs.sendMessage(tabId, message, function(response)
+		{
+			if(callback) callback(response);
+		});
+	});
+}
+
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    sendMessageToContentScript({onUpdated:true}, (response) => {
+        if(response) console.log(response);
+    });
+});
